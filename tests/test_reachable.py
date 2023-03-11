2edfa9eff6dcb48175a9bfd99b37a7bfbb7b18a4
Administrator <amihaiba@gmail.com>
2023-03-11 23:57:55 +0200

    Update test_reachable.py

--- tests/test_reachable.py
+++ tests/test_reachable.py
@@ -1,3 +1,6 @@
+# Worksheet   : 
+# Author      : Amihai Ben-Arush
+# Description : Determine if the web app is up and running by sending a request and getting a 2xx status code
 import pytest
 import requests
 
